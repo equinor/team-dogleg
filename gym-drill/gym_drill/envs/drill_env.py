c9f058d6dea682bd99e9b5b6540deacd77aaf689
GjerdeV <gjerde.martin@hotmail.com>
2020-06-30 10:05:43 +0200

    Fixing importing-issue

--- gym-drill/gym_drill/envs/drill_env.py
+++ gym-drill/gym_drill/envs/drill_env.py
@@ -3,34 +3,8 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 import matplotlib.pyplot as plt
-#import customAdditions as ca
-
-# A cartesian coordinate
-class Coordinate:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-
-    def getX(self):
-        return self.x
-
-    def getY(self):
-        return self.y
-    
-    # For displaying the coordinate
-    def __str__(self):
-        return "(" + str(self.x) + "," + str(self.y) + ")"
-
-    # For boolean comparison (if myPoint == yourPoint)
-    def __eq__(self, other):
-        return self.y == other.y and self.x == other.x
-
-    @classmethod
-    def getEuclideanDistance(cls,p1,p2):
-        return np.abs(np.sqrt((p1.x - p2.x)**2 + (p1.y-p2.y)**2))
-
-def isWithinTraget(bitPosition,targetPosition,targetRadius):
-    return (bitPosition.x - targetPosition.x)**2 + (bitPosition.y - targetPosition.y)**2 < targetRadius**2
+from gym_drill.envs.customAdditions import Coordinate
+from gym_drill.envs.customAdditions import isWithinTraget
 
 def all_visited(list):
     for i in range(len(list)):
