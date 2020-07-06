8c8717febff710531edf2586ef949ccd5b44117a
Jakob Vahlin <jakobvahlin@gmail.com>
2020-07-06 14:45:39 +0200

    Remove name from target as it was only used for testing

--- gym-drill/gym_drill/envs/Target.py
+++ gym-drill/gym_drill/envs/Target.py
@@ -6,11 +6,8 @@ class TargetBall():
         self.radius = rad
         
         self.reached = False # Indicate that the target has been reached.
-        self.name = "Have no name yet"
-    
-    def set_name(self,name):
-        self.name = name
-        
+        self.is_hit = False
+
     # Print the target to console. Can be nice for debugging purposes
     def __str__(self):
         return "Center:" + str(self.center) +". Radii:" + str(self.radius)
