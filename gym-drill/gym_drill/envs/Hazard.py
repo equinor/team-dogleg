6f2dabf14608c21891bd8ba5ff8f4c6d197f779b
Jakob Vahlin <jakobvahlin@gmail.com>
2020-07-06 14:50:35 +0200

    Add simple Hazard class

---
+++ gym-drill/gym_drill/envs/Hazard.py
@@ -0,0 +1,13 @@
+from gym_drill.envs.Coordinate import Coordinate
+
+HAZARD_PENALTY = -100 #?
+# Currently Hazard is the opposite of reward
+class Hazard:
+    def __init__(self,x,y,rad):
+        self.center = Coordinate(x,y)
+        self.radius = rad
+
+        self.penalty = HAZARD_PENALTY
+
+    def __str__(self):
+        return "Center:" + str(self.center) +". Radii:" + str(self.radius)
