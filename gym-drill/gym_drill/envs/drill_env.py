88c8ea4c4477ef1f896c00db6a77c5a63750c5a6
Jakob Vahlin <jakobvahlin@gmail.com>
2020-07-07 10:47:11 +0200

    Resovle import issue by moving support files back to env file

--- gym-drill/gym_drill/envs/drill_env.py
+++ gym-drill/gym_drill/envs/drill_env.py
@@ -3,14 +3,13 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 import matplotlib.pyplot as plt
-import environment_support
 
 # Our own libs
 from gym_drill.envs.Coordinate import Coordinate
 from gym_drill.envs.ObservationSpace import ObservationSpace
 from gym_drill.envs.Target import TargetBall
-import gym_drill.envs.environment_support as es
-from gym_drill.envs.environment_support import *
+#import gym_drill.envs.environment_support as es
+#from gym_drill.envs.environment_support import *
 
 
 
@@ -64,7 +63,7 @@ class DrillEnv(gym.Env):
         self.initialAngAcc = bitInitialization[2]
 
         # Init targets. See _init_targets function
-        self.targets = es._init_targets(NUM_TARGETS,TARGET_BOUND_X,TARGET_BOUND_Y,TARGET_RADII_BOUND,startLocation)
+        self.targets = _init_targets(NUM_TARGETS,TARGET_BOUND_X,TARGET_BOUND_Y,TARGET_RADII_BOUND,startLocation)
         
         self.action_space = spaces.Discrete(3)        
          
@@ -183,7 +182,7 @@ class DrillEnv(gym.Env):
         self.step_history = [[self.start_x,self.start_y]]       
 
         # Need to init new targets
-        self.targets = es._init_targets(NUM_TARGETS,TARGET_BOUND_X,TARGET_BOUND_Y,TARGET_RADII_BOUND,self.bitLocation)             
+        self.targets = _init_targets(NUM_TARGETS,TARGET_BOUND_X,TARGET_BOUND_Y,TARGET_RADII_BOUND,self.bitLocation)             
 
         self.state = self.get_state()
 
@@ -252,7 +251,7 @@ class DrillEnv(gym.Env):
             y_positions.append(position[1])       
 
         
-        # Plot circles from targetballs, just to verify the order of the balls
+        # Plot circles from targetballs, colors just to verify the order of the balls
         theta = np.linspace(0, 2*np.pi, 100)
         colors_order = {
             1:"b",
@@ -264,16 +263,10 @@ class DrillEnv(gym.Env):
             7:"k"
             }
         cnt = 1
-        t = 0
         for target in self.targets:
-            t += 1
             center = target.center
-            radius = target.radius
-            
-            d = Coordinate.getEuclideanDistance(Coordinate(x_positions[0],y_positions[0]),target.center)
-            print("Distance from start to target #",t, "is: ",d)         
-
-
+            radius = target.radius          
+                           
             x = center.x + radius*np.cos(theta)
             y = center.y + radius*np.sin(theta)
 
@@ -289,4 +282,74 @@ class DrillEnv(gym.Env):
         plt.title("Well trajectory path")
 
         plt.show()
+
+
+# Finds nearest between 1 point and a list of candidate points
+# startlocation is type Coordinate, and candidates is list of types Targets
+def _findNearest(start_location,candidates):
+    current_shortest_distance = -1 # Init with an impossible distance
+    current_closest_target_index = 0
+    for candidate_index in range(len(candidates)):        
+        candidate = candidates[candidate_index]     
+        distance = Coordinate.getEuclideanDistance(candidate.center,start_location)
+        
+        if distance < current_shortest_distance or current_shortest_distance == -1:           
+            current_shortest_distance = distance
+            current_closest_target_index = candidate_index
+        
+    return current_closest_target_index
+
+# Orders the target based upon a given start location
+# start_location is type Coordinate, all_targets is list of type targets
+def _orderTargets(start_location,all_targets):
+    #target_order = [None] * len(all_targets) # Maybe better with = [] and use append()
+    target_order = [] 
+    loop_counter = 0    
+
+    while len(all_targets) != 0:
+        if loop_counter == 0:
+            next_in_line_index = _findNearest(start_location,all_targets)
+            next_in_line_target = all_targets[next_in_line_index]
+            target_order.append(next_in_line_target)
+            all_targets.pop(next_in_line_index)
+        else:
+            next_in_line_index = _findNearest(target_order[loop_counter-1].center,all_targets)
+            next_in_line_target = all_targets[next_in_line_index]
+            target_order.append(next_in_line_target)
+            all_targets.pop(next_in_line_index)  
+
+        loop_counter += 1  
+
+    return target_order
+
+# Returns an ordered list of randomly generated targets within the bounds given. 
+def _init_targets(num_targets,x_bound,y_bound,r_bound,start_location):
+    all_targets = []
+
+    for t in range(num_targets):
+        target = _create_unique_random_target(x_bound,y_bound,r_bound,all_targets)
+        all_targets.append(target)        
+    
+    all_targets = _orderTargets(start_location,all_targets)
+
+    return all_targets
+
+# Returns True if t1 or t2 overlap 
+def _is_overlapping(t1,t2):
+    total_radii = t1.radius + t2.radius
+    distance = Coordinate.getEuclideanDistance(t1.center,t2.center)
+    return  distance < total_radii
+
+# Creates a uniqe target that does not overlap with any targets in existing_targets
+def _create_unique_random_target(x_bound,y_bound,r_bound,existing_targets):
+    target_center = Coordinate(np.random.uniform(x_bound[0],x_bound[1]),(np.random.uniform(y_bound[0],y_bound[1] )))
+    target_radius = np.random.uniform(r_bound[0],r_bound[1])
+    target_candidate = TargetBall(target_center.x,target_center.y,target_radius)
+
+    for target in existing_targets:
+        if _is_overlapping(target,target_candidate):
+            target_candidate =_create_unique_random_target(x_bound,y_bound,r_bound,existing_targets)
+            break
+
+    return target_candidate
 
