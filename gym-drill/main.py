350802d21b8e5855b2a1808883dc2b0e8e3d87ee
Jakob Vahlin <jakobvahlin@gmail.com>
2020-07-16 22:53:28 +0200

    Add benchmark function to test trained model in specific env

--- gym-drill/main.py
+++ gym-drill/main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 
 from gym_drill.envs.Coordinate import Coordinate
 from gym_drill.envs.Policies import CustomPolicy
+from gym_drill.envs import environment_config as cfg
 
 from stable_baselines.common import make_vec_env
 from stable_baselines.common.vec_env import DummyVecEnv
@@ -19,115 +20,132 @@ import warnings
 import tensorflow as tf
 warnings.simplefilter(action='ignore', category=FutureWarning)
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-
-#Setting up the environment
-STARTLOCATION = Coordinate(100,900.0)
-BIT_INITIALIZATION = [random.uniform(np.pi/2,np.pi),0.0,0.0] #initial heading is also set to random in the reset function (drill_env.py)
+warnings.simplefilter(action='ignore', category=Warning)
 
 
-env_name = 'drill-v0'
-env = gym.make(env_name,startLocation = STARTLOCATION, bitInitialization = BIT_INITIALIZATION, activate_hazards=True)
+def main():
+	#Setting up the environment
+	STARTLOCATION = Coordinate(cfg.SCREEN_X*0.1,cfg.SCREEN_Y*0.8)
+	BIT_INITIALIZATION = [random.uniform(np.pi/2,np.pi),0.0,0.0] #initial heading is also set to random in the reset function (drill_env.py)
 
-print("Obs space", env.observation_space)
-print("action space", env.action_space)
+	env_name = 'drill-v0'
+	env = gym.make(env_name,startLocation = STARTLOCATION, bitInitialization = BIT_INITIALIZATION, activate_hazards=True)
 
-#Using Stable-Baselines to teach an agent 
+	print("Obs space", env.observation_space)
+	print("action space", env.action_space)
 
-#DQN-approach
+	#Using Stable-Baselines to teach an agent 
 
-model_to_load = "DQN_drill_model_hazards"
-save_as = "DQN_drill_model_hazards"
-tensorboard_folder = "./algorithm_performance_hazards/"
-tensorboard_run_name = "DQN"
-#Chose one of the two lines below (#1 or #2):
-model = DQN(LnMlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)           #1) Make a new model
-#model = DQN.load(model_to_load, env, exploration_initial_eps=0.02, learning_rate= 0.0005, tensorboard_log=tensorboard_folder)              #2) Load an existing one from your own files
-print("DQN: I start training now")
-model.learn(total_timesteps=1000000, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
+	#DQN-approach
 
-"""
-#PPO2-approach
+	model_to_load = "DQN_drill_model_hazards"
+	save_as = "DQN_drill_model_hazards"
+	tensorboard_folder = "./algorithm_performance_hazards/"
+	tensorboard_run_name = "DQN"
+	#Chose one of the two lines below (#1 or #2):
+	model = DQN(LnMlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)           #1) Make a new model
+	#model = DQN.load(model_to_load, env, exploration_initial_eps=0.02, learning_rate= 0.0005, tensorboard_log=tensorboard_folder)              #2) Load an existing one from your own files
+	print("DQN: I start training now")
+	model.learn(total_timesteps=1000000, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
 
-model_to_load = "PPO2_drill_model"
-save_as = "PPO2_drill_model"
-tensorboard_folder = "./algorithm_performance_comparison/"
-tensorboard_run_name = "PP02"
-#Chose one of the two lines below (#1 or #2):
-model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
-#model = PPO2.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
-print("PPO2: I start training now")
-model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
+	"""
+	#PPO2-approach
 
-#A2C-approach
+	model_to_load = "PPO2_drill_model"
+	save_as = "PPO2_drill_model"
+	tensorboard_folder = "./algorithm_performance_comparison/"
+	tensorboard_run_name = "PP02"
+	#Chose one of the two lines below (#1 or #2):
+	model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
+	#model = PPO2.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
+	print("PPO2: I start training now")
+	model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
 
-model_to_load = "A2C_drill_model"
-save_as = "A2C_drill_model"
-tensorboard_folder = "./algorithm_performance_comparison/"
-tensorboard_run_name = "A2C"
-env = make_vec_env(env_name,n_envs=4)
-#Chose one of the two lines below (#1 or #2):
-model = A2C(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
-#model = A2C.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
-print("A2C: I start training now")
-model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
+	#A2C-approach
 
-#ACER-approach
+	model_to_load = "A2C_drill_model"
+	save_as = "A2C_drill_model"
+	tensorboard_folder = "./algorithm_performance_comparison/"
+	tensorboard_run_name = "A2C"
+	env = make_vec_env(env_name,n_envs=4)
+	#Chose one of the two lines below (#1 or #2):
+	model = A2C(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
+	#model = A2C.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
+	print("A2C: I start training now")
+	model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
 
-model_to_load = "ACER_drill_model"
-save_as = "ACER_drill_model"
-tensorboard_folder = "./algorithm_performance_comparison/"
-tensorboard_run_name = "ACER"
-env = make_vec_env(env_name,n_envs=4)
-#Chose one of the two lines below (#1 or #2):
-model = ACER(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
-#model = ACER.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
-print("ACER: I start training now")
-model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
+	#ACER-approach
 
-#ACKTR-approach
+	model_to_load = "ACER_drill_model"
+	save_as = "ACER_drill_model"
+	tensorboard_folder = "./algorithm_performance_comparison/"
+	tensorboard_run_name = "ACER"
+	env = make_vec_env(env_name,n_envs=4)
+	#Chose one of the two lines below (#1 or #2):
+	model = ACER(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              #1) Make a new model
+	#model = ACER.load(model_to_load, env, tensorboard_log=tensorboard_folder)               #2) Load an existing one from your own files
+	print("ACER: I start training now")
+	model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
 
-model_to_load = "ACKTR_drill_model"
-save_as = "ACKTR_drill_model"
-tensorboard_folder = "./algorithm_performance_comparison/"
-tensorboard_run_name = "ACKTR"
-env = make_vec_env(env_name,n_envs=4)
-#Chose one of the two lines below (#1 or #2):
-model = ACKTR(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              	#1) Make a new model
-#model = ACKTR.load(model_to_load, env, tensorboard_log=tensorboard_folder)                 #2) Load an existing one from your own files
-print("ACKTR: I start training now")
-model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
-"""
-#TRPO-approach 
-"""
-model_to_load = "TRPO_drill_model_1000"
-save_as = "TRPO_drill_model_1000"
-tensorboard_folder = "./algorithm_performance_comparison_1000/"
-tensorboard_run_name = "TRPO"
-#Chose one of the two lines below (#1 or #2):
-model = TRPO(MlpPolicy, env, verbose=0, tensorboard_log=tensorboard_folder)              	#1) Make a new model
-#model = TRPO.load(model_to_load, env, tensorboard_log=tensorboard_folder)                 #2) Load an existing one from your own files
-print("TRPO: I start training now")
-model.learn(total_timesteps=2000000, tb_log_name = tensorboard_run_name) #Where the learning happens
-model.save(save_as) #Saving the wisdom for later 
-"""
+	#ACKTR-approach
 
-print("Im done training and I will show you the results")
-#Show the result of the training
-obs = env.reset()
-for episode in range (3):
-	done = False
-	while done == False:
-		action, _states = model.predict(obs)
-		obs, rewards, done, info = env.step(action)
-	env.display_environment()
-	state = env.reset()
-	
-	env.close()
-	print('[EPISODE ENDED]')
+	model_to_load = "ACKTR_drill_model"
+	save_as = "ACKTR_drill_model"
+	tensorboard_folder = "./algorithm_performance_comparison/"
+	tensorboard_run_name = "ACKTR"
+	env = make_vec_env(env_name,n_envs=4)
+	#Chose one of the two lines below (#1 or #2):
+	model = ACKTR(MlpPolicy, env, verbose=1, tensorboard_log=tensorboard_folder)              	#1) Make a new model
+	#model = ACKTR.load(model_to_load, env, tensorboard_log=tensorboard_folder)                 #2) Load an existing one from your own files
+	print("ACKTR: I start training now")
+	model.learn(total_timesteps=100, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
+	"""
+	#TRPO-approach 
+	"""
+	model_to_load = "TRPO_drill_model_1000"
+	save_as = "TRPO_drill_model_1000"
+	tensorboard_folder = "./algorithm_performance_comparison_1000/"
+	tensorboard_run_name = "TRPO"
+	#Chose one of the two lines below (#1 or #2):
+	model = TRPO(MlpPolicy, env, verbose=0, tensorboard_log=tensorboard_folder)              	#1) Make a new model
+	#model = TRPO.load(model_to_load, env, tensorboard_log=tensorboard_folder)                 #2) Load an existing one from your own files
+	print("TRPO: I start training now")
+	model.learn(total_timesteps=2000000, tb_log_name = tensorboard_run_name) #Where the learning happens
+	model.save(save_as) #Saving the wisdom for later 
+	"""
 
-print("done")
+	print("Im done training and I will show you the results")
+	#Show the result of the training
+	obs = env.reset()
+	for episode in range (3):
+		done = False
+		while done == False:
+			action, _states = model.predict(obs)
+			obs, rewards, done, info = env.step(action)
+		env.display_environment()
+		state = env.reset()
+		
+		env.close()
+		print('[EPISODE ENDED]')
+
+	print("done")
+
+def benchmark_environment(targets,hazards,model,*,startpos=Coordinate(cfg.SCREEN_X*0.1,cfg.SCREEN_Y*0.8), bit_data=[random.uniform(np.pi/2,np.pi),0.0,0.0],num_runs = 1):    	
+    env = gym.make('drill-v0',startLocation = startpos, activate_hazards=True)
+    env.load_predefined_env(targets,hazards)
+
+    obs = np.array(env.get_state())
+    done = False
+    while not done:
+        action,states = model.predict(obs)    
+        obs, rewards, done, info = env.step(action)
+    env.display_environment()
+    env.close()
+
+if __name__ == '__main__':
+	main()
