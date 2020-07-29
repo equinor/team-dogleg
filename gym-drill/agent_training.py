8d8e9b49f181f2ecb368a688236deae4028e8b47
Martin H. Myrestrand <martin_myrestrand@hotmail.com>
2020-07-29 10:30:00 +0200

    set gamma = 0.999

--- gym-drill/agent_training.py
+++ gym-drill/agent_training.py
@@ -42,7 +42,7 @@ TENSORBOARD_FOLDER_PPO2 = "../tensorboard_logs/PPO2/"
 
 def train_new_DQN(total_timesteps,save_name):
 	print("Starting DQN training session of",total_timesteps,"timesteps...")
-	model = DQN(LnMlpPolicy, ENV, verbose=1, learning_rate=0.0003, exploration_fraction=0.2,policy_kwargs=policy_kwargs,exploration_final_eps=0.0,tensorboard_log=TENSORBOARD_FOLDER_DQN)
+	model = DQN(LnMlpPolicy, ENV, verbose=1, learning_rate=0.0003,gamma=0.999, exploration_fraction=0.2,policy_kwargs=policy_kwargs,exploration_final_eps=0.0,tensorboard_log=TENSORBOARD_FOLDER_DQN)
 	model.learn(total_timesteps=total_timesteps, tb_log_name = "DQN")
 	print("Done training with DQN algorithm.")
 	save_model(model,save_name)
